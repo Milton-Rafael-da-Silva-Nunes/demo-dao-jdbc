53b32086ee4d15ce95fd434b068d144516ee60d8
rafael_nunes <rafa.nuns17@gmail.com>
2023-09-08 19:35:06 -0300

    Departamento entidade Classe

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departamento dep = new Departamento(1, "Books");
+		
+		System.out.println(dep);
+		
+
+	}
+
+}
