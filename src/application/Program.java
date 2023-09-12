a9eb122528b8fc4f45866b4b8b8033fc0a4dbdd7
rafael_nunes <rafa.nuns17@gmail.com>
2023-09-11 21:02:45 -0300

    Implementando findbyDepartamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
 
+import java.util.List;
+
 import model.dao.FabricaDao;
 import model.dao.FuncionarioDao;
+import model.entities.Departamento;
+import model.entities.Funcionario;
 
 public class Program {
 
@@ -13,7 +17,15 @@ public class Program {
 		System.out.println("=== TESTE 1: Funcionario findById ===");
 		funcionariodao.findById(1);
 		System.out.println(funcionariodao.findById(1));
-
+		
+		System.out.println();
+		
+		System.out.println("=== TESTE 2: Funcionario findByDepartamento ===");
+		Departamento departamento = new Departamento(2, null);
+		List<Funcionario> list = funcionariodao.findByDepartamento(departamento);
+		for(Funcionario obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/dao/impl/FuncionarioDaoJDBC.java
+++ src/model/dao/impl/FuncionarioDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -63,7 +66,7 @@ public class FuncionarioDaoJDBC implements FuncionarioDao {
 	}
 
 	private Funcionario instaciacaoFuncionario(ResultSet rs, Departamento dep) throws SQLException {
-		Funcionario obj =  new Funcionario();
+		Funcionario obj = new Funcionario();
 		obj.setId(rs.getInt("id"));
 		obj.setNome(rs.getString("name"));
 		obj.setEmail(rs.getString("email"));
@@ -85,4 +88,44 @@ public class FuncionarioDaoJDBC implements FuncionarioDao {
 		return null;
 	}
 
+	@Override
+	public List<Funcionario> findByDepartamento(Departamento departamento) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " 
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id " 
+					+ "WHERE DepartmentId = ? " 
+					+ "ORDER BY Name");
+
+			st.setInt(1, departamento.getId());
+			rs = st.executeQuery();
+
+			List<Funcionario> lista = new ArrayList<>();
+			Map<Integer, Departamento> map = new HashMap<>();
+
+			while (rs.next()) { 
+				
+				Departamento dep = map.get(rs.getInt("DepartmentId")); 
+				
+				if (dep == null) {
+					dep = instaciacaoDepartamento(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Funcionario obj = instaciacaoFuncionario(rs, dep);
+				lista.add(obj);
+			}
+			return lista;
+
+		} catch (SQLException e) {
+			throw new DbException("Error ao buscar funcionario por departamento: " + e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 }
